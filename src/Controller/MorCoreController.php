876bbdd4fc059fcf66a525c33557845662ae7faa
Maarten de Keizer <github@maartendekeizer.nl>
2025-03-24 20:08:38 +0100

    Add logger

--- src/Controller/MorCoreController.php
+++ src/Controller/MorCoreController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
